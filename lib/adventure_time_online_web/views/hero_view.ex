52133b46b1404623a1dfaf64f2261ad762a1cf5b
Adam Wenham <adamwenham64@gmail.com>
2021-08-30 16:00:33 +0200

    more color cases for game tiles

--- lib/adventure_time_online_web/views/hero_view.ex
+++ lib/adventure_time_online_web/views/hero_view.ex
@@ -1,7 +1,7 @@
 defmodule AdventureTimeOnlineWeb.HeroView do
   use AdventureTimeOnlineWeb, :view
 
-  alias AdventureTime.GameTile
+  alias AdventureTime.{GameTile, HeroServer}
 
   def header("new.html", _assigns) do
     ~e{Create a Hero!}
@@ -16,16 +16,24 @@ defmodule AdventureTimeOnlineWeb.HeroView do
       game_tile.walkable == false ->
         ~e{<td class="w-12 h-12 border-2 border-gray-900 bg-gray-700"></td>}
 
+      GameTile.hero_on_tile?(game_tile.tile_ref, hero_name) &&
+          HeroServer.alive?(hero_name) == false ->
+        ~e{<td class="w-12 h-12 border-2 border-gray-900 bg-indigo-500"></td>}
+
       GameTile.hero_count(game_tile.tile_ref) > 1 && GameTile.heroes_on_tile?(game_tile.tile_ref) &&
-          GameTile.hero_on_tile?(game_tile.tile_ref, hero_name) ->
+        GameTile.hero_on_tile?(game_tile.tile_ref, hero_name) &&
+          GameTile.alive_enemies_on_tile?(game_tile.tile_ref, hero_name) ->
         ~e{<td class="w-12 h-12 border-2 border-gray-900 bg-yellow-500"></td>}
 
-      GameTile.hero_count(game_tile.tile_ref) == 1 &&
-          GameTile.hero_on_tile?(game_tile.tile_ref, hero_name) ->
+      GameTile.hero_on_tile?(game_tile.tile_ref, hero_name) ->
         ~e{<td class="w-12 h-12 border-2 border-gray-900 bg-green-500"></td>}
 
-      GameTile.heroes_on_tile?(game_tile.tile_ref) ->
+      GameTile.heroes_on_tile?(game_tile.tile_ref) &&
+          GameTile.anyone_alive_on_tile?(game_tile.tile_ref) ->
         ~e{<td class="w-12 h-12 border-2 border-gray-900 bg-red-500"></td>}
+
+      GameTile.heroes_on_tile?(game_tile.tile_ref) ->
+        ~e{<td class="w-12 h-12 border-2 border-gray-900 bg-blue-500"></td>}
 
       true ->
         ~e{<td class="w-12 h-12 border-2 border-gray-900"></td>}
